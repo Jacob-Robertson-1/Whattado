0db337a68e78e4ea73ad23270f776b0de2756720
Jacob Robertson <jwrob1026@gmail.com>
2015-05-17 15:41:28 -0600

    linked front end with back end

--- public/controllers/mainControl.js
+++ public/controllers/mainControl.js
@@ -2,5 +2,21 @@ var app = angular.module("whattado")
 
 app.controller("mainControl", function() {
 
+  $scope.getParseData = function() {
+    parseService.getData().then(function(response) {
+        $scope.messages = response.data.results;
+        console.log(response);
+      },
+      function(err) {
+        console.log(err)
+      })
+  };
+
+  $scope.getParseData();
+  $scope.postData = function() {
+    mainService.postData($scope.firstName)
+    mainService.postData($scope.lastName)
+    $scope.message = '';
+  };
 
 });
--- public/services/mainService.js
+++ public/services/mainService.js
@@ -4,4 +4,25 @@ var app = angular.module("whattado");
 app.service("mainService", function() {
 
 
+this.postData = function(yourName) {
+  return $http({
+    method: 'POST',
+    url: 'mongodb://localhost:27017/Whattado',
+    data: {
+      FirstName: "",
+      lastName: ""
+    }
+  })
+}
+
+this.getData = function() {
+  return $http({
+    method: 'GET',
+    url: 'mongodb://localhost:27017/Whattado'
+  });
+}
+
+});
+
+
 });
